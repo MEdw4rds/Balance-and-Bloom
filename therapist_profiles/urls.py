a3dba7a0319d2a5dc3c6498c31e8a50ddfeb29b4
Caprice Holford <capricehxo@gmail.com>
2025-01-15 13:18:31 +0000

    updating profile html and making it look nice, slowly getting there

--- therapist_profiles/urls.py
+++ therapist_profiles/urls.py
@@ -4,15 +4,16 @@ from . import views
 from therapist_profiles import views as index_views
 from django.conf import settings
 from django.conf.urls.static import static
+from therapist_profiles import views as therapist_views
 
 urlpatterns = [
-path('admin/', admin.site.urls),
-    path('', index_views.index, name='index'),
-    path('profile/', views.create_or_update_profile, name='create_or_update_profile'),
-    path('profile/<str:username>/', views.view_profile_by_username, name='view_profile_by_username'),
-    path('profile/<int:user_id>/', views.view_profile_by_id, name='view_profile_by_id'),
+    path('', therapist_views.index, name='index'),
+    path('profile/', therapist_views.create_or_update_profile, name='create_or_update_profile'),
+    path('profile/<str:username>/', therapist_views.view_profile_by_username, name='view_profile_by_username'),
+    path('profile/<int:user_id>/', therapist_views.view_profile_by_id, name='view_profile_by_id'),
+    path('therapist_profile/', therapist_views.view_profile, name='therapist_profile'),
     path('accounts/', include('allauth.urls')),
-    path('', views.create_or_update_profile, name='create_or_update_profile'),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
