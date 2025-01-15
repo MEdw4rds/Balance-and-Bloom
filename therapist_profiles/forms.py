bfacefb30c6194c2ebf6030abf7938ddb36b0b99
Caprice Holford <capricehxo@gmail.com>
2025-01-15 15:20:31 +0000

    fixed therapist profile not loading issue, made editing changes

--- therapist_profiles/forms.py
+++ therapist_profiles/forms.py
@@ -1,11 +1,16 @@
 from django import forms
-from .models import TherapistProfile, Availability
+from .models import TherapistProfile, Availability, Service
 
 
 class TherapistProfileForm(forms.ModelForm):
+    services = forms.ModelMultipleChoiceField(
+        queryset=Service.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
     class Meta:
         model = TherapistProfile
-        fields = ['bio', 'credentials', 'services', 'photo'] 
+        fields = ['bio', 'credentials', 'services', 'photo']
 
 
 class AvailabilityForm(forms.ModelForm):
