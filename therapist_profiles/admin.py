966409278519ab099a884ad1ca6d3633b24f8b71
Matthew Edwards <wste993l@students.codeinstitute.net>
2025-01-24 14:51:19 +0000

    update navbar links and move about and contact to footer format files for better readability

--- therapist_profiles/admin.py
+++ therapist_profiles/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import TherapistProfile, Availability, Service
 
+
 # Define Availability as an Inline Admin
 class AvailabilityInline(admin.TabularInline):  # Use StackedInline for vertical layout if preferred
     model = Availability
     extra = 1  # Number of empty rows for adding new availability
     can_delete = True
+
 
 class TherapistProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'bio', 'credentials', 'get_services', 'photo')
@@ -15,9 +17,11 @@ class TherapistProfileAdmin(admin.ModelAdmin):
         return ", ".join([service.name for service in obj.services.all()])
     get_services.short_description = 'Services Offered'
 
+
 # Customize Availability Admin (Optional if you want to keep it separate too)
 class AvailabilityAdmin(admin.ModelAdmin):
     list_display = ('therapist', 'day', 'start_time', 'end_time')
+
 
 # Register models
 admin.site.register(TherapistProfile, TherapistProfileAdmin)
